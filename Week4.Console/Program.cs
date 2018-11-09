967fa127bf70da5e3df3a5b0fed2d088814ef3fa
ITSPaul <powell.paul@itsligo.ie>
2018-11-09 13:05:31 +0000

    New Console test methods added and rememebr reference to the connnection string associated with the Context has to in the app.config for a console app in order to find the database correctly

--- Week4.Console/Program.cs
+++ Week4.Console/Program.cs
@@ -13,8 +13,13 @@ namespace Week4.Console
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+
+            AssignRandomStudentsToMemberlessClubs();
+            //ListClubs();
+
             List<Programme> programmes = new List<Programme>();
                 // Get the current assembly
                 Assembly assembly = Assembly.GetExecutingAssembly();
@@ -66,6 +71,46 @@ namespace Week4.Console
                 c++;
             }
             System.Console.ReadKey();
+        }
+
+        public static void AssignRandomStudentsToMemberlessClubs()
+        {
+            ClubContext ctx = new ClubContext();
+
+            List<Club> ClubWithNoMembers = ctx.Clubs
+                .Where(club => club.clubMembers.Count() < 1)
+                .ToList();
+
+            if (ClubWithNoMembers.Count() > 0)
+            {
+                foreach (var club in ClubWithNoMembers)
+                    addMemberstoClub(ctx, club);
+            }
+            System.Console.ReadKey();
+        }
+        private static void addMemberstoClub(ClubContext ctx, Club club)
+        {
+            List<string> RandomSIDs = ctx.Students.Select(
+                      s => new { s.StudentID, gid = Guid.NewGuid() })
+                      .OrderBy(g => g.gid)
+                      .Select(s => s.StudentID)
+                      .ToList();
+            List<ClubDomain.Classes.ClubModels.Student> sublist =
+                    ctx.Students.Where(s => RandomSIDs.Contains(s.StudentID)).Take(3).ToList();
+            foreach (var item in sublist)
+                //ClubWithNoMembers.clubMembers.Add(new Member { });
+                System.Console.WriteLine("Adding {0} to Club ", item.StudentID, club.ClubId);
+        }
+
+        public static void ListClubs()
+        {
+            using (ClubContext clubContext = new ClubContext())
+            {
+                foreach (var item in clubContext.Clubs)
+                {
+                    System.Console.WriteLine(item.ClubName);
+                }
+            }
         }
     }
 }
